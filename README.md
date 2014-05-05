987fbb7118fedeb56789ec55996175f8845f56b3
Steve Huff <shuff@runkeeper.com>
2014-05-05 16:37:59 -0400

    first working version
    still need to implement EB restart hook

---
+++ README.md
@@ -0,0 +1,10 @@
+This app is the sample app from the [Tomcat 7 documentation](https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/), used for testing.
+
+## Instructions
+
+1. Install the [AWS command-line tools](https://aws.amazon.com/cli/).
+2. Install some reasonable JDK.
+3. Look in **deploy.sh** to see the environment variable you'll need to set.
+4. `./deploy.sh`
+5. ...
+6. Profit!
---
+++ deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Prerequisites:
+#
+# 1. The `aws` executable in your path.
+# 2. The `jar` executable in your path.
+# 3. The following environment variables exported in your shell:
+#    $GITHUB_API_TOKEN  : GitHub API token (cf. https://github.com/blog/1270-easier-builds-and-deployments-using-git-over-https-and-oauth)
+
+# test for environment
+if [[ -z $GITHUB_API_TOKEN ]]; then
+  echo 'No value found for $GITHUB_API_TOKEN!'
+  exit 1
+fi
+
+# write the EB extension
+cd $(dirname "${BASH_SOURCE[0]}")
+
+EXTDIR='example/.ebextensions'
+
+mkdir -p $EXTDIR
+
+cat > "${EXTDIR}/01bootstrap.config" <<EBBOOTSTRAP
+---
+packages:
+  yum:
+    git: []
+    puppet: []
+  python:
+    awscli: []
+    boto-rsync: []
+EBBOOTSTRAP
+
+cat > "${EXTDIR}/02mean_bean.config" <<EBMEANBEAN
+---
+commands:
+  01rmdir:
+    command: "rm -rf /opt/mean-bean-s3log-machine"
+    cwd: "/opt"
+  02mkdir:
+    command: "mkdir -p /opt/mean-bean-s3log-machine"
+    cwd: "/opt"
+  03init:
+    command: "git init"
+    cwd: "/opt/mean-bean-s3log-machine"
+  04pull:
+    command: "git pull https://${GITHUB_API_TOKEN}:x-oauth-basic@github.com/FitnessKeeper/mean-bean-s3log-machine.git"
+    cwd: "/opt/mean-bean-s3log-machine"
+  05apply:
+    command: "puppet apply --modulepath=./modules manifests/default.pp"
+    cwd: "/opt/mean-bean-s3log-machine"
+EBMEANBEAN
+
+# make the WAR
+jar -cvf example.war example
+
+# version string
+if [ $? ]; then
+  echo "Version: $(date +%s)"
+fi
